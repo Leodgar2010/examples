972b2d1c1089aca679cc43d2abfd6e8a057cb296
Leodgar2010 <leodgar2010@yandex.ru>
2022-08-03 21:46:08 +0300

    Program for Hellouser

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine ("Enter your name: ");
+string username = Console.ReadLine ();
+Console.WriteLine ("Hello " + username);
