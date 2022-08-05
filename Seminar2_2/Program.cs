cc44bbe85ec4952071b92d7d7973b6e9cf6c582d
Leodgar2010 <leodgar2010@yandex.ru>
2022-08-05 21:43:46 +0300

    Исправил вторую задачу

--- Seminar2_2/Program.cs
+++ Seminar2_2/Program.cs
@@ -5,27 +5,31 @@ if (number1==1)
 {
    Console.WriteLine ("Понедельник");
 }
-if (number1==2)
+else if (number1==2)
 {
    Console.WriteLine ("Вторник");
 }
-if (number1==3)
+else if (number1==3)
 {
    Console.WriteLine ("Среда");
 }
-if (number1==4)
+else if (number1==4)
 {
    Console.WriteLine ("Четверг");
 }
-if (number1==5)
+else if (number1==5)
 {
    Console.WriteLine ("Пятница");
 }
-if (number1==6)
+else if (number1==6)
 {
    Console.WriteLine ("Суббота");
 }
-if (number1==7)
+else if (number1==7)
 {
    Console.WriteLine ("Воскресенье");
+}
+else 
+{
+    Console.WriteLine ("Неверный номер дня недели");
 }
