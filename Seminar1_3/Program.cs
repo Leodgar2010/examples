5f92bc05d13e619fcda552c6032ee349a99fe4a7
Leodgar2010 <leodgar2010@yandex.ru>
2022-08-06 08:52:35 +0300

    Исправили третье задание

---
+++ Seminar1_3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine ("Введите число: ");
+string? numberString1 = Console.ReadLine ();
+int number1=  int.Parse (numberString1);
+if (number1 < 0)
+{
+    number1 = (number1* -1);
+}    
+int number2=(number1*-1);
+Console.WriteLine ("Результат:");
+Console.WriteLine (number2);
+while (number2 < number1)
+{
+    number2=number2+1;
+    Console.WriteLine (number2);
+
+}
