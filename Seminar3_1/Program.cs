38ba5e32105c8e1bb76948ce3f16bed2a9379718
Leodgar2010 <leodgar2010@yandex.ru>
2022-08-12 22:05:02 +0300

    Третий семинар

---
+++ Seminar3_1/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine ("Вычисление четверти, в которой находится точка");
+Console.WriteLine ("Введите координату X:");
+var xString = Console.ReadLine ();
+int x= int.Parse (xString!);
+if (x==0) 
+{
+    Console.WriteLine ("Невозможно вычислить четверть для точки, лежащей на оси");
+    return;
+}
+Console.WriteLine ("Введите координату y:");
+var yString = Console.ReadLine ();
+int y= int.Parse (yString!);
+if (y== 0) 
+{
+    Console.WriteLine ("Невозможно вычислить четверть для точки, лежащей на оси");
+    return;
+}    
+int result = 0;
+if (x>0 && y>0)
+    result = 1;
+else if (x<0 && y>0)
+    result = 2;
+else if (x<0 && y<0)
+    result = 3;
+else if (x>0 && y<0)
+    result = 2;
+if (result!=0)
+
+    Console.WriteLine ("Номер четверти в которой находится точка: "+result);
+else
+    Console.WriteLine ("Невозможно вычислить четверть для точки, лежащей на оси");
---
+++ Seminar3_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход
+// координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+Console.WriteLine("Введите координату X точки A:");
+string? numberXAstring = Console.ReadLine();
+int numberXA = int.Parse(numberXAstring!);
+Console.WriteLine("Введите координату Y точки A:");
+string? numberYAstring = Console.ReadLine();
+int numberYA = int.Parse(numberYAstring!);
+Console.WriteLine("Введите координату X точки B:");
+string? numberXBstring = Console.ReadLine();
+int numberXB = int.Parse(numberXBstring!);
+Console.WriteLine("Введите координату Y точки B:");
+string? numberYBstring = Console.ReadLine();
+int numberYB = int.Parse(numberYBstring!);
+
+double numberXX = numberXB - numberXA;
+double numberYY = numberYB - numberYA;
+double result = (double) Math.Sqrt ((numberXX * numberXX) + (numberYY * numberYY));
+Console.WriteLine("Расстояние между точками А и В равно: " + result + ".");
