039da8a9411d9756264a8f7307b436d08345f813
Leodgar2010 <leodgar2010@yandex.ru>
2022-08-14 14:38:45 +0300

    Вторая лекция про методы

---
+++ Example013_TextTransform/Program.cs
@@ -0,0 +1,14 @@
+﻿string text = "Scarabey";
+string Replace (string text, char oldValue, char newValue)
+{
+string result = string.Empty;
+int length = text.Length;
+for (int i = 0; i<length; i++)
+    {
+        if (text [i] == oldValue) result=result + $"{newValue}";
+        else result= result + $"{text[i]}";
+    }
+return result;
+}
+string newText = Replace (text,'c', 'a');
+Console.WriteLine (newText);
