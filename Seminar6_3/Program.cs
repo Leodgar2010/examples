7eb4f4c3094daebade1bff91326d5ea2232018d1
Leodgar2010 <leodgar2010@yandex.ru>
2022-08-26 21:34:26 +0300

    Седьмой семинар

--- Seminar6_3/Program.cs
+++ Seminar6_3/Program.cs
@@ -1,36 +1,44 @@
-﻿//Задача 42: Напишите программу, 
-//которая будет преобразовывать 
-//десятичное число в двоичное.
-//45 -> 101101
-//3  -> 11
-//2  -> 10
-int a=number1;
-int b=0;
-int c=0;
-string DecimalToBinary (int input)
-{
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+string DecimalToBinary(int input) {
     var result = string.Empty;
-    var divider =2;
-    int resultLength = 0;
+    var divider = 2;
+
+    int resultLength = 1;
     int temp = input;
-while (input/divider >0)
-{
-    resultLenghth++;
-    temp = temp/divider;
+
+    while(temp / divider > 0) {
+        resultLength++;
+        temp /= divider;
+    }
+
+    Console.WriteLine(resultLength);
+
+    int[] resultArray = new int[resultLength]; 
+
+    int index = resultLength - 1;
+    while(index > 0) {
+        resultArray[index] += input % divider;
+        index--;
+        input /= divider;
+        Console.WriteLine(resultArray[index]);
+    }
+    resultArray[0] = input;
+
+    for (int i = 0; i < resultArray.Length; i++)
+        result += resultArray[i];
+
+    return result;
 }
 
-    result += int
-   
+Console.WriteLine("Введите 10-чное число");
+if(!int.TryParse(Console.ReadLine()!, out var number)) {
+    Console.WriteLine("Всё плохо");
+} 
 
-    a = (a/2);
-    b=a%2;
-    c=b/2;
-    Console.Write (b);
-}
-}    
-Console.WriteLine ("Введите число: ");
-if (!int.TryParse (ConsoleReadline()!, out var number))
-{ 
-    Console.WriteLine ("Всё плохо");
-}
-var result = DecimalToBinary (number);
+var result = DecimalToBinary(number);
+
+Console.WriteLine("Двоичное представление числа " + number + " есть число " + result);
---
+++ Seminar7_1/Program.cs
@@ -0,0 +1,28 @@
+﻿void PrintArray (int [,] array)
+{
+   for (int i=0; i<result.GetLength(0);i++)
+    {
+        for (int j=0; j<result.GetLength(1);j++) Console.Write ($"{array[i,j]}");
+    }
+    Console.WriteLine ();    
+}
+int [,] CreateArrayWithRandomNumbers (int m, int n)
+{
+    int random = new Random ();
+    int [,] result = new int [m,n]();
+    for (int i=0; i<result.GetLength(0);i++)
+    {
+        for (int j=0; j<result.GetLength(1);j++) result [i,j] = random.Next (1,100);
+    }
+    return result
+}
+
+Console.WriteLine ("Введите число строк (m)");
+if(!int.TryParse(Console.ReadLine()!, out var m)) {
+    Console.WriteLine("Всё плохо");
+}
+Console.WriteLine ("Введите число cтолбцов (n)");
+if(!int.TryParse(Console.ReadLine()!, out var n)) {
+    Console.WriteLine("Всё плохо");
+}
+
